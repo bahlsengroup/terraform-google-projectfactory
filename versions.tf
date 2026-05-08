d78b3ebef75ff61b72acda4746d16a4985c1a835
Christian Schroer <christian.schroer@btelligent.com>
2026-05-08 08:47:43 +0200

    chore(deps): allow terraform provider v7

--- versions.tf
+++ versions.tf
@@ -1,4 +1,4 @@
-# Copyright 2025 Bahlsen GmbH & Co. KG
+# Copyright 2026 Bahlsen GmbH & Co. KG
 # Copyright 2025 METRO Digital GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = ">= 6.0, < 8.0"
     }
   }
   required_version = ">= 1.12"

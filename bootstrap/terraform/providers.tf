6df61bfd6af9e8a8d9bccb6135b9921c80a82cee
robert.greil <robert.greil@btelligent.com>
2026-06-18 12:49:30 +0200

    fix: update providers, fix .gitignore

--- bootstrap/terraform/providers.tf
+++ bootstrap/terraform/providers.tf
@@ -17,20 +17,20 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = ">= 6.0, <8.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.0"
+      version = ">= 6.0, <8.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.4.0"
+      version = ">= 2.5.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.9.1"
+      version = ">= 0.10"
     }
   }
-  required_version = ">= 1.10"
+  required_version = ">= 1.13"
 }
